3217425ced3b624f2e858993ae19bc4ea5df9da9
cthorax <cthorax@users.noreply.github.com>
2018-07-30 21:34:56 -0700

    still untested, refactoring for db.py need to be finished.

--- predict.py
+++ predict.py
@@ -27,13 +27,12 @@ def lookup(alias, db):
         
     return id
     
-def make_prediction_series(alias, date, db, number_of_history_matches=10):
+def make_prediction_series(alias, event_date, db=29991231, number_of_history_matches=10):
     from math import log10, ceil
     id = lookup(alias, db)
     
     index_list = ['wrestler_id', 'dob', 'nationality']
-    index_template_list = ['days_since_match', 'won_match', 'wintype_match', 'title_match', 'matchtype_match', 'opponents_match', 
-                 'allies_match']
+    index_template_list = ['days_since_match', 'won_match', 'wintype_match', 'title_match', 'matchtype_match', 'opponents_match', 'allies_match']
     padding = int(ceil(log10(number_of_history_matches)))
     for number in range(number_of_history_matches):
         padded_number = str(number).zfill(padding)
@@ -45,14 +44,14 @@ def make_prediction_series(alias, date, db, number_of_history_matches=10):
     
     global max_team_size
     global max_competitors
+    wrestler_query = "SELECT id FROM teams_table WHERE wrestler00 = '{id}'".format(id=id)
     for wrestler_number in range(1, max_team_size):
-        wrestler_query = "SELECT id FROM teams_table WHERE wrestler00 = '{id}'".format(id=id)
         wrestler_query_template = "wrestler{number} = '{id}'"
-        wrestler_query = " OR ".join([wrestler_query, wrestler_query_template.format(id=id, number=str(wrestler_number).zfill(2))
+        wrestler_query = " OR ".join([wrestler_query, wrestler_query_template.format(id=id, number=str(wrestler_number).zfill(2))])
                                       
     teams_query = db.query(wrestler_query).as_dict()
     competitors_query = db.query("SELECT id FROM competitors_table WHERE teamXX = '{id}'".format(id=id)).as_dict()
-    history_query = db.query("SELECT date, wintype, title, matchtype, competitors WHERE date < {date} AND competitors IN (\nSELECT id FROM competitors_table WHERE  ) FROM match_table ORDER BY date DESC".format()).as_dict()
+    history_query = db.query("SELECT date, wintype, title, matchtype, competitors WHERE date < {event_date} AND competitors IN (\nSELECT id FROM competitors_table WHERE  ) FROM match_table ORDER BY date DESC".format()).as_dict()
 
     prediction_series['id'] = id
     prediction_series['dob'] = stats_query.get('dob',0)
